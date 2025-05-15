6854d4da20b95b1428318a0d6b55208c58a53852
The Repos <ry4nyeo@gmail.com>
2025-05-15 20:00:13 +0800

    updates

--- docs/extra/scroll-offset.js
+++ docs/extra/scroll-offset.js
@@ -1,7 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var shiftWindow = function() {
-    window.scrollBy(0, -100); // adjust -100 based on your header height
-  };
-  if (location.hash) shiftWindow();
-  window.addEventListener("hashchange", shiftWindow);
+document.addEventListener("DOMContentLoaded", () => {
+  const HEADINGS = document.querySelectorAll("main h1, main h2, main h3");
+  const OFFSET = 100; // Adjust if your sticky header is larger/smaller
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      for (const entry of entries) {
+        if (entry.isIntersecting) {
+          history.replaceState(null, null, `#${entry.target.id}`);
+        }
+      }
+    },
+    {
+      rootMargin: `-${OFFSET}px 0px 0px 0px`,
+      threshold: 1.0,
+    }
+  );
+
+  HEADINGS.forEach((h) => {
+    if (h.id) observer.observe(h);
+  });
 });
