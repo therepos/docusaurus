77b9cb3a0fc9095f4aa41d430a0d1acf44e04755
therepos <therepos@users.noreply.github.com>
2025-05-15 12:00:31 +0000

    deploy: 6854d4da20b95b1428318a0d6b55208c58a53852

--- extra/scroll-offset.088246e7.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var shiftWindow = function() {
-    window.scrollBy(0, -100); // adjust -100 based on your header height
-  };
-  if (location.hash) shiftWindow();
-  window.addEventListener("hashchange", shiftWindow);
-});
