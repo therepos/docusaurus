0fddd1ab11c24b774e22de4867891bf860bafdde
The Repos <ry4nyeo@gmail.com>
2025-05-30 00:15:03 +0800

    updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,7 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 
+const currentYear = new Date().getFullYear();
+
 export default {
   title: 'Docusaurus',
   tagline: 'Docusaurus Tagline',
