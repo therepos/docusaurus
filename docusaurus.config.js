8830ee599aad02fbecc4bae28c73bc7abdb48901
The Repos <ry4nyeo@gmail.com>
2025-06-05 14:43:07 +0800

    updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -100,3 +100,91 @@ export default {
   },
 };
 
+// =============================================
+// Use the following for blogless single sidebar
+// ---------------------------------------------
+// 1. Delete /blog, /pages, /data, readme.md
+// 2. Set /docs/index.md
+// 3. Modify sidebars.js
+// =============================================
+
+// import {themes as prismThemes} from 'prism-react-renderer';
+
+// const currentYear = new Date().getFullYear();
+// const org = process.env.ORG_NAME;
+// const repo = process.env.PROJECT_NAME;
+
+// export default {
+//   title: process.env.SITE_TITLE,
+//   tagline: 'Docusaurus Tagline',
+//   url: process.env.SITE_URL,
+//   baseUrl: process.env.BASE_URL,
+//   organizationName: process.env.ORG_NAME,
+//   projectName: process.env.PROJECT_NAME,
+//   deploymentBranch: 'gh-pages',
+//   favicon: 'img/favicon.ico',
+//   trailingSlash: false,
+
+//   presets: [
+//     [
+//       '@docusaurus/preset-classic',
+//       {
+//         docs: {
+//           path: 'docs',
+//           routeBasePath: '/',
+//           sidebarPath: './sidebars.js',
+//           showLastUpdateTime: true,
+//           sidebarCollapsible: true,
+//           editUrl: `https://github.com/${org}/${repo}/edit/main/`,
+//         },
+//         theme: {
+//           customCss: './src/css/styles.css',
+//         },
+//       },
+//     ],
+//   ],
+
+//   themeConfig: {
+//     navbar: {
+//       title: 'Docs',
+//       items: [
+//         {
+//           type: 'search',
+//           position: 'right',
+//         },
+//         {
+//           href: `https://github.com/${org}/${repo}`,
+//           position: 'right',
+//           className: 'header-github-link',
+//           'aria-label': 'GitHub repository',
+//         },
+//       ],
+//     },
+//     docs: {
+//       sidebar: {
+//         hideable: true,
+//         autoCollapseCategories: true,
+//       },
+//     },
+//     prism: {
+//       theme: prismThemes.github,
+//     },
+//     footer: {
+//       style: 'dark',
+//       links: [],
+//       copyright: `
+//         <div class="footer-row">
+//           <div class="footer-left">
+//             <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank" style="color: #ebedf0;">CC BY 4.0</a> © ${currentYear} therepos.<br/>
+//             Made with Docusaurus.
+//           </div>
+//           <div class="footer-icons">
+//             <a href="https://github.com" class="icon icon-github" target="_blank" aria-label="GitHub"></a>
+//             <a href="https://hub.docker.com" class="icon icon-docker" target="_blank" aria-label="Docker"></a>
+//           </div>
+//         </div>
+//       `,
+//     },
+//   },
+// };
+
