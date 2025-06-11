e3eadc4b827a1839f246cdb7f2c0e5c562f822fa
The Repos <ry4nyeo@gmail.com>
2025-06-11 17:48:52 +0800

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,3 +50,24 @@ function AnimatedCard({ title, link, description }) {
     </motion.div>
   );
 }
+
+// ===============================
+// For redirects
+// ===============================
+
+// import { useEffect } from 'react';
+// import { useHistory } from '@docusaurus/router';
+// import useBaseUrl from '@docusaurus/useBaseUrl';
+
+// export default function Redirect() {
+//   const history = useHistory();
+//   const target = useBaseUrl('/');  
+//   // use /blog for redirect to blog
+//   // no slug default to lowercase folder name
+  
+//   useEffect(() => {
+//     history.replace(target);
+//   }, [target]);
+
+//   return null;
+// }
