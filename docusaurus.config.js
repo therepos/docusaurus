bffa10c7aba78ca7d64983087129727db68bcc5e
The Repos <ry4nyeo@gmail.com>
2025-06-13 04:49:40 +0800

    up

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,6 +3,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 const currentYear = new Date().getFullYear();
 const org = process.env.ORG_NAME;
 const repo = process.env.PROJECT_NAME;
+const sharedFooter = require('./sharedFooter');
 
 export default {
   title: process.env.SITE_TITLE,
@@ -81,25 +82,7 @@ export default {
     prism: {
       theme: prismThemes.github,
     },
-    footer: {
-      style: 'dark',
-      links: [],
-      copyright: `
-        <div class="footer-row">
-          <div class="footer-left">
-            <a href="https://creativecommons.org/licenses/by/4.0/" target="_blank" style="color: #ebedf0;">CC BY 4.0</a> © ${currentYear} ${org}.<br/>
-            Made with Docusaurus.
-          </div>
-          <div class="footer-icons">
-            <a href="https://github.com/therepos?tab=repositories" class="icon icon-github" target="_blank" aria-label="GitHub"></a>
-            <a href="https://hub.docker.com" class="icon icon-docker" target="_blank" aria-label="Docker"></a>
-            <a href="https://therepos.github.io/proxmox/" class="icon icon-proxmox" target="_blank" aria-label="Proxmox"></a>
-            <a href="https://therepos.github.io/msexcel/" class="icon icon-excel" target="_blank" aria-label="Excel"></a>
-            <a href="https://therepos.github.io/docusaurus/" class="icon icon-docusaurus" target="_blank" aria-label="Excel"></a>            
-          </div>
-        </div>
-      `,
-    },
+    footer: sharedFooter,
   },
 };
 
