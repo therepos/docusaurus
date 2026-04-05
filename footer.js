028c925b9027bd5d598c9681a6e20325178fe869
The Repos <ry4nyeo@gmail.com>
2026-04-06 02:58:25 +0800

    up

--- footer.js
+++ footer.js
@@ -11,9 +11,6 @@ export default {
         Made with Docusaurus.
       </div>
       <div class="footer-icons">
-        <a href="https://github.com/therepos?tab=repositories" class="icon icon-github" target="_blank" aria-label="GitHub"></a>
-        <a href="https://therepos.github.io/proxmox/" class="icon icon-proxmox" target="_blank" aria-label="Proxmox"></a>
-        <a href="https://therepos.github.io/rpa/" class="icon icon-chrome" target="_blank" aria-label="RPA"></a>    
         <a href="https://archives-nine.vercel.app/" class="icon icon-notes" target="_blank" aria-label="Notes"></a>        
       </div>
     </div>
